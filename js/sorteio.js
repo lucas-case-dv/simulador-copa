b1d449909933398cece0f24e736eec6a5fc5a989
lucas-case-dv <106892666+lucas-case-dv@users.noreply.github.com>
2026-04-23 19:39:06 -0300

    fix: remove embaralhamento desnecessário dos times

--- js/sorteio.js
+++ js/sorteio.js
@@ -6,18 +6,7 @@ const times = await buscarTimes();
 
 function sortearGrupos() {
 
-    //Embaralha os 32 times recebidos
-    function embaralhar(array) {
-        for (let i = array.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]];
-        }
-        return array;
-    }
-
-    const timesEmbaralhados = embaralhar(times);
-
-    //Divide os times embaralhos em 8 grupos de 4
+    //Divide os times em 8 grupos de 4
     function separarGrupos(array, tamanho) {
         const pedacos = [];
         for (let i = 0; i < array.length; i += tamanho) {
@@ -26,8 +15,8 @@ function sortearGrupos() {
         return pedacos;
     }
 
-    const grupos = separarGrupos(timesEmbaralhados, 4);
-
+    const grupos = separarGrupos(times, 4);
+    return grupos;
 }
 
-sortearGrupos();
+console.log(sortearGrupos());
