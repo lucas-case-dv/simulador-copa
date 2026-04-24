82c12c91846e70fbad4bfbbe20ddf613a68e7abb
lucas-case-dv <106892666+lucas-case-dv@users.noreply.github.com>
2026-04-23 22:45:58 -0300

    feat: simula resultados da fase de grupos

--- js/simulacao.js
+++ js/simulacao.js
@@ -28,4 +28,42 @@ function criarPartidas() {
     return todosOsJogos;
 }
 
+function simularGrupos(cronograma) {
+    cronograma.forEach(itemGrupo => {
+        //Percorre cada partida de um grupo
+        itemGrupo.jogos.forEach(partida => {
+            processarResultado(partida);
+        });
+    });
+}
+
+function processarResultado(partida) {
+    // Gera gols aleatórios de 0 a 5
+    const golsA = Math.floor(Math.random() * 6);
+    const golsB = Math.floor(Math.random() * 6);
+
+    partida.placarA = golsA;
+    partida.placarB = golsB;
+
+    partida.timeA.golsMarcados += golsA;
+    partida.timeA.golsSofridos += golsB;
+    partida.timeA.saldoGols = partida.timeA.golsMarcados - partida.timeA.golsSofridos;
+
+    partida.timeB.golsMarcados += golsB;
+    partida.timeB.golsSofridos += golsA;
+    partida.timeB.saldoGols = partida.timeB.golsMarcados - partida.timeB.golsSofridos;
+
+    if (golsA > golsB) {
+        partida.timeA.pontos += 3;
+    } else if (golsB > golsA) {
+        partida.timeB.pontos += 3;
+    } else {
+        partida.timeA.pontos += 1;
+        partida.timeB.pontos += 1;
+    }
+}
+
 const cronograma = criarPartidas();
+simularGrupos(cronograma);
+console.log(grupos);
+
